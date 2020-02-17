2923e1d14c084535e04dd276dc40b7e5a894482a
w1bll3 <will@willdeane.co.uk>
2020-02-17 20:50:40 +0000

    first alpha - finds user associated with access key - need to complete parsing out group and perms

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # aakat
 AWS Access Key Audit Tool
+
+
+#### To Do
+1. If access key is found enumerate permissions
+2. If access key isn't found - search cloudtrail fo events related to access key ID
---
+++ aakat.py
@@ -0,0 +1,55 @@
+import boto3
+import argparse
+import re
+
+
+def validate_access_key_id(key_id):
+    # RegEx pattern for AWS Access Key ID
+    # taken from https://aws.amazon.com/blogs/security/a-safer-way-to-distribute-aws-credentials-to-ec2/
+    key_id_pattern = re.compile('(?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])')
+    if not key_id_pattern.match(key_id):
+        msg = "Supplied Access Key ID doesn't match AWS Access Key ID Format"
+        raise argparse.ArgumentTypeError(msg)
+    else:
+        return (key_id)
+
+
+# Parse group names out of dict returned by iam_client.list_groups_for_user(UserName='user')
+def parse_groups(AWS_group_dict):
+    group_info = None
+    print("bla")
+    return group_info
+
+
+# Get parameters
+parser = argparse.ArgumentParser(description=
+                                 "Search AWS account for Access Key ID and return user and "
+                                 "permission details if found.\n"
+                                 "Uses default aws credentials as per AWS CLI")
+parser.add_argument('aws_access_key_id', type=validate_access_key_id, help='Specify AWS Access Key ID')
+args = parser.parse_args()
+
+# Create IAM client/ resource
+iam_client = boto3.client('iam')
+iam_resource = boto3.resource('iam')
+
+aws_user = None
+for user in iam_resource.users.all():
+    for key in user.access_keys.all():
+        if args.aws_access_key_id == key.id:
+            print("Access Key ID {0} belongs user to user {1}".format(key.id, key.user_name))
+            aws_user = key.user_name
+
+# If access key found, enumerate permissions for user
+if aws_user:
+    aws_groups = iam_client.list_groups_for_user(UserName=aws_user)
+    aws_inline_polices = iam_client.list_user_policies(UserName=aws_user)
+    aws_managed_polices = iam_client.list_attached_user_policies(UserName=aws_user)
+    users_groups = parse_groups(aws_groups)
+    if aws_inline_polices['PolicyNames']:
+        print(aws_inline_polices)
+    # print("Managed Policies")
+    if aws_managed_polices['AttachedPolicies']:
+        print(aws_managed_polices)
+else:
+    print("Access key {0} not found.".format(args.aws_access_key_id))
