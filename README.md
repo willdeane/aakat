5640017b70231715960219cf6ccc2ed2cba92a6b
w1bll3 <will@willdeane.co.uk>
2020-03-02 18:12:16 +0000

    Added functionality to search cloudtrail for access key

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # aakat
-### Access Key Audit Tool
-Use case: AWS Access key has been found, e.g., in a git repo, temporary file, or environment variables and you need
-to identify which user the access key is associated with.
+### AWS Access Key Audit Tool
+Use case: An AWS Access key has been found, e.g., in a git repo, temporary file, or environment variables and you need
+to identify which user the access key is associated with. 
 
+If the Access Key is active, you have both the access key ID and the secret key, and you have permission to use the keys
+it's more efficient to use the AWS CLI instead:
+
+`aws sts get-caller-identity`
+
+`aakat` will first search all IAM users, if the key isn't associated with any user it will search the last 
+90 days of CloudTrail logs for any event associated with the Access Key ID. 
+ 
 If the AWS access key is found it prints:
 1. The username
 2. The names of any groups the user is a member of
@@ -10,10 +18,7 @@ If the AWS access key is found it prints:
 4. The names of any polices attached to the user
 
 
-
-
 #### To Do
-1. If access key isn't found - search cloudtrail for events related to access key ID 
-2. Enumerate permission for groups, inline polices and attached polices
-3. produced 'consolidated' permission set by combining all permissions in all associated polices 
+1. Enumerate permission for groups, inline polices and attached polices and print policy json
+2. Produced 'consolidated' permission set by combining all permissions in all associated polices 
  
--- aakat.py
+++ aakat.py
@@ -1,6 +1,8 @@
 import boto3
 import argparse
 import re
+import json
+import time
 
 
 def validate_access_key_id(key_id):
@@ -60,46 +62,76 @@ parser.add_argument('--aws-profile',
 
 args = parser.parse_args()
 
-# Create IAM client/ resource
+
+# Create AWS client/ resource
 if args.profile:
     boto3.setup_default_session(profile_name=args.profile)
 iam_client = boto3.client('iam')
 iam_resource = boto3.resource('iam')
+ec2_client = boto3.client('ec2')
+#cloudtrail_client = boto3.client('cloudtrail')
 
+# get a list of AWS regions, exclude disabled regions
+regions = []
+response = ec2_client.describe_regions(AllRegions=False)
+for i in response['Regions']:
+    regions.append(i['RegionName'])
+
+
+# search users for access key ID
 aws_user = None
 for user in iam_resource.users.all():
     for key in user.access_keys.all():
         if args.aws_access_key_id == key.id:
-            print("Access Key ID {0} belongs user to user {1}".format(key.id, key.user_name))
+            print(f"Access Key ID {key.id} belongs user to user {key.user_name}")
             aws_user = key.user_name
+
+if not aws_user:
+    #  search CloudTrail for access key activity: CloudTrail only searches previous 90 days
+    for region in regions:
+        print(f"Checking Region {region} for events.")
+        cloudtrail_client = boto3.client('cloudtrail', region_name=region)
+        response = cloudtrail_client.lookup_events(LookupAttributes=[{'AttributeKey': 'AccessKeyId', 'AttributeValue':args.aws_access_key_id}, ], MaxResults=1)
+        if (len(response['Events'])) > 0:
+            data = json.loads(response['Events'][0]['CloudTrailEvent'])
+            print(
+                f"CloudTrail record found in region {region}: "
+                f"Access Key ID: {args.aws_access_key_id} "
+                f"is associated with user: {data['userIdentity']['userName']}")
+            aws_user = data['userIdentity']['userName']
+            break
+        else:
+            continue
+        break
+
 
 # If access key found, enumerate permissions for user
 if aws_user:
     # Group details
     users_groups = parse_groups(iam_client.list_groups_for_user(UserName=aws_user))
     if users_groups:
-        print("\nUser {0} is a member of the following groups:".format(aws_user))
+        print(f"\nUser {aws_user} is a member of the following groups:")
         for g in users_groups:
             print(g)
     else:
-        print("\nUser {0} is not a member of any groups.".format(aws_user))
+        print(f"\nUser {aws_user} is not a member of any groups.")
 
     # Inline policy details
     users_inline_polices = parse_inline_polices(iam_client.list_user_policies(UserName=aws_user))
     if users_inline_polices:
-        print("\nUser {0} has the following inline polices:".format(aws_user))
+        print(f"\nUser {aws_user} has the following inline polices:")
         for inline in users_inline_polices:
             print(inline)
     else:
-        print("\nUser {0} is not a has no inline polices.".format(aws_user))
+        print(f"\nUser {aws_user} is not a has no inline polices.")
 
     # Managed Policy details
     users_attached_polices = parse_attached_polices(iam_client.list_attached_user_policies(UserName=aws_user))
     if users_attached_polices:
-        print("\nUser {0} has the following attached policies:".format(aws_user))
+        print(f"\nUser {aws_user} has the following attached policies:")
         for attached in users_attached_polices:
             print(*attached, sep="\t")
     else:
-        print("\nUser {0} is not a has no attached polices.".format(aws_user))
+        print(f"\nUser {aws_user} is not a has no attached polices.")
 else:
-    print("Access key {0} not found.".format(args.aws_access_key_id))
+    print(f"Access key {args.aws_access_key_id} not found.")
